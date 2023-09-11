4cc1dbe6df869aa6c2940ff0d4411bfb6ac8e7e2
DavidEvans <david@evanstmd.plus.com>
2023-09-11 12:06:52 +0100

    Ready to use APIs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 //const x = require("./script2")
 const fruitForm = document.querySelector("#inputSection form")
-fruitForm.addEventListener("submit, e => console.log(e")
+const fruitList = document.querySelector("#fruitSection ul")
+const trash = document.querySelector(".fa-trash");
 
-console.log(fruitForm)
+const delFruit = () => {
+    document.querySelector("li:hover").remove()
+}
+
+const addFruit = fruit =>{
+    if (!fruit){
+        return null
+    }
+    const li = document.createElement("li")
+    li.textContent = fruit 
+    li.addEventListener("click", removeFruit, {once:true})
+    fruitList.appendChild(li)
+}
+
+const removeFruit = e => {
+    e.target.remove()
+}
+fruitForm.addEventListener("submit", e => {
+    e.preventDefault()
+    addFruit(e.target.fruitInput.value)
+    e.target.fruitInput.value = "   "
+})
+
+
+
+
 //console.log(`The answer is ${x}`)
