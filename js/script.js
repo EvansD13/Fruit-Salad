f46d11ecebbe59906927bc36d4748c5a30e44f7f
DavidEvans <david@evanstmd.plus.com>
2023-09-11 11:04:43 +0100

    Several updates, mainly to js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//const x = require("./script2")
+const fruitForm = document.querySelector("#inputSection form")
+fruitForm.addEventListener("submit, e => console.log(e")
+
+console.log(fruitForm)
+//console.log(`The answer is ${x}`)
